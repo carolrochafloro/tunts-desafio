4f761175a95200dff71ce4b5ebb9096ecd12bc89
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-10 09:53:25 -0300

    Created authorization function

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -3,38 +3,26 @@ const path = require('path');
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
+const scopes = require('./config.js');
 
-// If modifying these scopes, delete token.json.
-const{ sheetId, scopes} = require('./config.js');
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+/* Authorization function. If there's a token.json in the directory it'll load it. If there isn't one yet, it'll create a new file after login via browser. */
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
+const tokenPath = path.resolve(__dirname, 'token.json');
+const credentialsPath = path.resolve(__dirname, 'credentials.json');
+
 async function loadSavedCredentialsIfExist() {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
+    const content = await fs.readFile(tokenPath);
     const credentials = JSON.parse(content);
     return google.auth.fromJSON(credentials);
   } catch (err) {
+    console.error('No credentials found:', err );
     return null;
   }
 }
 
-/**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
 async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
+  const content = await fs.readFile(credentialsPath);
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
@@ -43,13 +31,10 @@ async function saveCredentials(client) {
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
   });
-  await fs.writeFile(TOKEN_PATH, payload);
+  await fs.writeFile(tokenPath, payload);
 }
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
+
 async function authorize() {
   let client = await loadSavedCredentialsIfExist();
   if (client) {
@@ -57,7 +42,7 @@ async function authorize() {
   }
   client = await authenticate({
     scopes,
-    keyfilePath: CREDENTIALS_PATH,
+    keyfilePath: credentialsPath,
   });
   if (client.credentials) {
     await saveCredentials(client);
@@ -65,26 +50,5 @@ async function authorize() {
   return client;
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-async function getDataSheet(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: 'A4:F29',
-  });
-  const rows = res.data.values;
-  if (!rows || rows.length === 0) {
-    console.log('No data found.');
-    return;
-  }
-  console.log('Data from sheet');
-  rows.forEach((row) => {
-    console.log(`${row[0]}, ${row[4]}`);
-  });
-}
 
-authorize().then(getDataSheet).catch(console.error);
+module.exports = authorize
