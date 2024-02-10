8c20e8e6f20a8bf4301e1278cb7723220a6efbab
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-09 23:07:10 -0300

    Primeira conexão configurada

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,5 @@
 const sheetId = "1g1McJwmlqukKte20fGJy8vkb_3kiEcni1IOVTLbA230";
 const scopes =  'https://www.googleapis.com/auth/spreadsheets.readonly';
-const tokenPath =  path.join(process.cwd(), 'token.json');
-const credentialsPath = path.join(process.cwd(), 'credentials.json');
+const range = ""
 
-module.exports = {sheetId, scopes, tokenPath, credentialsPath}
+module.exports = {sheetId, scopes}
