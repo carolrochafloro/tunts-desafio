c0465e363bc17de716762b626c29022237c167cf
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-10 10:42:05 -0300

    Ajuste - nome da variável

--- src/services/getSheetData.js
+++ src/services/getSheetData.js
@@ -5,11 +5,11 @@ const {google} = require('googleapis');
 
 async function getSheetData(auth, range) {
   const sheets = google.sheets({version: 'v4', auth});
-  const res = await sheets.spreadsheets.values.get({
+  const result = await sheets.spreadsheets.values.get({
     spreadsheetId: sheetId,
     range: range,
   });
-  const rows = res.data.values;
+  const rows = result.data.values;
   if (!rows || rows.length === 0) {
     console.log('No data found.');
     return;
