d5d0ce496bd89a0c055b81feedb3ad0ac4a3c5e7
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-10 09:53:10 -0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,4 @@
 const sheetId = "1g1McJwmlqukKte20fGJy8vkb_3kiEcni1IOVTLbA230";
 const scopes =  'https://www.googleapis.com/auth/spreadsheets.readonly';
-const range = ""
 
 module.exports = {sheetId, scopes}
