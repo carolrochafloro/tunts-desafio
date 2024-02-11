815ad32bc8459907874d6053c821a35ef0c57d88
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-11 15:47:37 -0300

    File index.js will run the application.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// index.js
+
+const absencesController = require('./controllers/absences.js');
+const gradesController = require('./controllers/grades.js');
+
+async function runApp() {
+  try {
+
+    await absencesController.absences();
+    console.log('Absences controller executed successfully.');
+
+    await gradesController();
+    console.log('Grades controller executed successfully.');
+
+    console.log('All controllers executed successfully.');
+  } catch (error) {
+    console.error('Error running controllers:', error);
+  }
+}
+
+runApp();
