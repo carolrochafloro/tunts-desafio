14cf8c4f083ae63d093a6398f3bf13b58db5e88f
carolrochafloro <carolinarochaf@hotmail.com>
2024-02-11 11:06:50 -0300

    Updated scoipes on config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 const sheetId = "1g1McJwmlqukKte20fGJy8vkb_3kiEcni1IOVTLbA230";
-const scopes =  'https://www.googleapis.com/auth/spreadsheets.readonly';
+const scopeRead =  'https://www.googleapis.com/auth/spreadsheets.readonly';
+const scopeModify = 'https://www.googleapis.com/auth/spreadsheets';
 
-module.exports = {sheetId, scopes}
+module.exports = {sheetId, scopeRead, scopeModify}
